c6e9e1df6d2df564ec876460d642a8831dd06584
marvintau <nightvista@gmail.com>
2020-04-28 20:59:29 +0800

    ueah

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,45 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-import ExampleComponent from 'use-fsm'
+import './index.css';
 
-export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
-  }
+import Form from '@marvintau/well-formed';
+
+
+const space = {
+  markedResend: {type: 'label', color: 'warning', desc:'已标记为重发函'},
+  sendID: {type: 'input', desc: '寄件运单号'},
+  recvID: {type: 'input', desc: '收件运单号'},
+  received: {type:'label', color:'info', desc:'已确认收到回函'},
+  alternative: {type: 'label', color: 'info', desc:'已进入替代程序'},
+  confirmed: {type:'label', color:'success', desc:'已确认金额相符'},
+  confirmedNE: {type: 'label', color:'danger', desc:'已确认金额不符'}
+}
+
+const states = {
+  init: {vec: {sendID: true}, enterDesc: '确认发函'},
+  sent: {vec: {sendID: true, recvID: true}, enterDesc:'确认发函'},
+  received: {vec: {received: true}, enterDesc: '收到回函', color:'success'},
+  requestedResend: {vec: {markedResend: true, sendID: true}, enterDesc:'重新发函', color:'warning'},
+  alternative: {vec: {alternative: true}, enterDesc:'进入替代程序'},
+  confirmed: {vec: {confirmed:true}, enterDesc:'确认金额相符'},
+  confirmedNE: {vec:{confirmedNE: true}, enterDesc:'确认金额不符'},
+}
+
+const rules = {
+  requestedResend: 'sent',
+  init: 'sent',
+  sent: [ 'received', 'requestedResend'],
+  received: [
+    'requestedResend',
+    'alternative',
+    'confirmed',
+    'confirmedNE',
+  ],
+  confirmed: {name:'init', enterDesc: '重置', color:'danger'},
+  confirmedNE: {name: 'init', enterDesc: '重置', color:'danger'},
+  alternative: {name: 'init', enterDesc: '重置', color:'danger'},
+}
+
+export default () => {
+  return <Form {...{space, states, rules}} />
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,132 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useRef } from 'react'
 
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+const useFSM = ({space, states, rules, formRef, initial='init'}) => {
+  
+  if (states[initial] === undefined){
+    throw Error(`State set must includes the initial state you specified, ${initial}`);
   }
 
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+  if (rules[initial] === undefined){
+    throw Error(`State transition rules must includes the initial state you specified, ${initial}`);
   }
+
+  const [curr, setCurr] = useState(states[initial]);
+  const [next, setNext] = useState(rules[initial]);
+  const [hist, setHist] = useState([]);
+
+  function valid(stateName) {
+    if (!(stateName in states)){
+      console.warn(`${stateName} does not appear in the state set: ${Object.keys(states)}`);
+      return false;
+    } else {
+      for (let comp of Object.keys(states[stateName].vec)){
+        if (!(comp in space)){
+          console.warn(`the Component ${comp} in State ${stateName} does not appear in the space: ${Object.keys(space)}`);
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  const proceed = (stateName) => {
+    if (!valid(stateName)){
+      console.warn(`invalid state name ${stateName}`);
+    } else {
+      setCurr(states[stateName]);
+      setNext(rules[stateName]);
+      setHist([...hist, stateName])
+    }
+  }
+
+  return [curr, next, hist, proceed];
+}
+
+const colorToStyle = (color) => {
+
+  return color
+  ? {
+    borderColor: `var(--${color})`,
+    color: `var(--${color})`,
+    background: `linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)), var(--${color}) `,
+  }
+  : {};
+}
+
+const Select = ({options, onSelect, valueKey='name', descKey="desc", defaultDesc="请选择"}) => {
+
+  const [isOpen, setOpen] = useState(false);
+  const [desc, setDesc] = useState(defaultDesc);
+
+  return <div className="select">
+    <div className="select-indicator" 
+      style={{borderBottom: isOpen ? '0.5px solid gray' : ''}}
+      onClick={() => {
+        console.log('click1')
+        setOpen(!isOpen);
+      }}
+    >{desc}</div>
+    {isOpen && <div className="select-options">
+      {options.map(({[valueKey]:value, [descKey]:desc, color}, i) => <div
+        className={`select-option ${color}`}
+        style={{color:`var(--${color})`}}
+        key={i}
+        onClick={() => {
+          onSelect(value);
+          setDesc(desc);
+          setOpen(false);
+        }}
+      >{desc}</div>)}
+    </div>}
+  </div>
+}
+
+const Input = ({name, desc}) => {
+  return <input className="form-input" type="text" name={name} placeholder={desc} />
+}
+
+export default ({space, states, rules}) => {
+  
+  const formRef = useRef(null);
+
+  const [{vec}, next, hist, goto] = useFSM({space, states, rules, formRef});
+
+  console.log(hist, 'hist');
+
+  let stateSwitch;
+  if (Array.isArray(next)){
+
+    const options = next.map(entry => {
+      if (typeof entry === 'string') {
+        const {enterDesc, color} = states[entry];
+        return {name: entry, enterDesc, color};
+      } else {
+        return entry;
+      }
+    });
+
+    const randKey = Math.random().toString(36).slice(2,7);
+
+    stateSwitch=<Select key={randKey} options={options} descKey='enterDesc' onSelect={(value) => {goto(value)}} />
+  } else if (typeof next === 'string') {
+    const {enterDesc, color} = states[next];
+    stateSwitch = <button className="form-button" style={colorToStyle(color)} onClick={() => goto(next)}>{enterDesc}</button>;
+  } else if (next.name) {
+    const {enterDesc, name, color} = next;
+    stateSwitch = <button className="form-button" style={colorToStyle(color)} onClick={() => goto(name)}>{enterDesc}</button>;
+  }
+
+  const elems = [];
+  for (let [name, {type, desc, color}] of Object.entries(space)) if(vec[name]){
+    if (type === 'input') {
+      elems.push(<Input key={name} {...{name, desc}} />)
+    } else if (type === 'label') {
+      elems.push(<div className="form-label" key={name} style={colorToStyle(color)}>{desc}</div>)
+    }
+  }
+
+  return <form className="form-holder" ref={formRef}>
+    {elems}
+    {stateSwitch}
+  </form>
 }
